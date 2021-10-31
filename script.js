aa7545dcaf669779187e8f626821dcb39163f7fb
solongee <sgee456@gmail.com>
2021-10-31 17:41:29 -0400

    basic menu functionality done

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+//namespace app
 const app = {};
+
+app.openMenu = document.querySelector('#openMenu');
+app.closeMenu = document.querySelector('#closeMenu');
 app.hamburgerMenu = document.querySelector('#hamburgerMenu');
+
+app.openMenu.addEventListener('click', () => {
+    app.openMenu.classList.toggle('hidden');
+    app.hamburgerMenu.classList.toggle('hidden');
+});
+
+app.closeMenu.addEventListener('click', () => {
+    app.openMenu.classList.toggle('hidden');
+    app.hamburgerMenu.classList.toggle('hidden');
+});
+
 app.init = function() {
     console.log(app.hamburgerMenu);
 };
-app.init();
+
+//document ready
+document.addEventListener('DOMContentLoaded', () => {
+	app.init()
+})
