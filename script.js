69e41499022e21be80674f7f39ad4a8ada6e771e
solongee <sgee456@gmail.com>
2021-10-31 18:26:57 -0400

    added transition effect to hamburger menu

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ app.hamburgerMenu = document.querySelector('#hamburgerMenu');
 app.openMenu.addEventListener('click', () => {
     app.openMenu.classList.toggle('hidden');
     app.hamburgerMenu.classList.toggle('hidden');
+    app.hamburgerMenu.classList.toggle('closed');
 });
 
 app.closeMenu.addEventListener('click', () => {
     app.openMenu.classList.toggle('hidden');
-    app.hamburgerMenu.classList.toggle('hidden');
+    app.hamburgerMenu.classList.toggle('closed');
+    //only changes visibility of menu back to hidden 
+    //after opacity and height transition is shown
+    setTimeout(() => {
+        app.hamburgerMenu.classList.toggle('hidden');
+    }, 400)
 });
 
 app.init = function() {
@@ -21,5 +27,5 @@ app.init = function() {
 
 //document ready
 document.addEventListener('DOMContentLoaded', () => {
-	app.init()
-})
+	app.init();
+});
