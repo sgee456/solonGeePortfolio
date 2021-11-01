948e76d02eb0ad80647a2470ee29f9ed75d72572
solongee <sgee456@gmail.com>
2021-11-01 00:34:19 -0400

    refactored script.js, removed stray console.log

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 //namespace app
 const app = {};
 
-app.openMenu = document.querySelector('#openMenu');
-app.closeMenu = document.querySelector('#closeMenu');
-app.hamburgerMenu = document.querySelector('#hamburgerMenu');
-
-app.openMenu.addEventListener('click', () => {
-    app.openMenu.classList.toggle('hidden');
-    app.hamburgerMenu.classList.toggle('hidden');
-    app.hamburgerMenu.classList.toggle('closed');
-});
-
-app.closeMenu.addEventListener('click', () => {
-    app.openMenu.classList.toggle('hidden');
-    app.hamburgerMenu.classList.toggle('closed');
-    //only changes visibility of menu back to hidden 
-    //after opacity and height transition is shown
-    setTimeout(() => {
-        app.hamburgerMenu.classList.toggle('hidden');
-    }, 400)
-});
 
 app.init = function() {
-    console.log(app.hamburgerMenu);
+    app.openMenu = document.querySelector('#openMenu');
+    app.closeMenu = document.querySelector('#closeMenu');
+    app.hamburgerMenu = document.querySelector('#hamburgerMenu');
+    
+    app.openMenu.addEventListener('click', () => {
+        app.openMenu.classList.toggle('hidden');
+        app.hamburgerMenu.classList.toggle('hidden');
+        app.hamburgerMenu.classList.toggle('closed');
+    });
+    
+    app.closeMenu.addEventListener('click', () => {
+        app.openMenu.classList.toggle('hidden');
+        app.hamburgerMenu.classList.toggle('closed');
+        //only changes visibility of menu back to hidden 
+        //after opacity and height transition is shown
+        setTimeout(() => {
+            app.hamburgerMenu.classList.toggle('hidden');
+        }, 400)
+    });
 };
 
 //document ready
